10199f3e93bbba5abb92300b86e10d560c13b442
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2023-09-30 18:44:38 +0000

    Implement account creation

---
+++ inputs.py
@@ -0,0 +1,66 @@
+from base64 import b64encode
+import re
+from getpass import getpass
+
+import bcrypt
+
+## Text constants
+icon_error = "❌"
+
+
+def question(prompt) -> str:
+    """Asks the user for input. Performs no input validation!"""
+    # This is a seperate function in case we want to add styling to the prompts
+    # in the future
+    return input(f"{prompt}")
+
+
+def text(prompt, error_message="Enter some text") -> str:
+    """Asks the user for input, ensuring that they've entered at least 1 character"""
+    raw_input = question(prompt)
+    if not raw_input:
+        print(f"{icon_error} {error_message}")
+        return text(prompt)
+
+    return raw_input
+
+
+def username(prompt) -> str:
+    """Usernames can be 1 to 64 characters. They must only be made up of word characters,
+    periods, hyphens or spaces."""
+    valid_username_regex = r"^[\w\.\- ]+$"
+
+    raw_input = text(prompt, "Enter a username")
+    length = len(raw_input)
+    if not 1 <= length <= 64:
+        print(f"{icon_error} Enter a username made up of 1–64 characters")
+        return username(prompt)
+    if not re.search(valid_username_regex, raw_input):
+        print(f"{icon_error} Only use letters, numbers, ., -, _, and spaces")
+        return username(prompt)
+
+    return raw_input
+
+
+def password_to_hash(password: str) -> str:
+    """Uses bcrypt to salt and hash the provided password, so that it can be stored safely.
+    Returns the password hash encoded in Base64 as a string."""
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return b64encode(hash).decode("utf-8")
+
+
+def password(prompt, hide_characters=True):
+    """A password must be at least 1 character, but has no other limitations.
+    Returns a hashed and salted version of the password.
+    By default, the typed text will be hidden from the user, but this can be disabled by setting hide_characters=False"""
+    if not hide_characters:
+        return password_to_hash(
+            text(prompt, "Enter a password to keep your account secure"))
+
+    raw_input = getpass(prompt)
+    if not raw_input:
+        print(f"{icon_error} Enter a password to keep your account secure")
+        return password(prompt, hide_characters)
+
+    return password_to_hash(prompt)
--- main.py
+++ main.py
@@ -1,22 +1,55 @@
 """Mr Leeman's System: A pupil management system for Tree Road School
 This code is for Task 3 of the lesson 2.2.1 Programming fundamentals - validation"""
 
+from colorama import Fore, Style
+from colorama import init as init_colorama
+
+import inputs
 from menu import Menu, Option, Submenu
 from util import JSONDatabase
 
 
 def log_in():
     print("Logging in")
-    print(accounts_database.data)
+    print(accounts)
     input()
 
 
+def create_account():
+    print(
+        "Your username will identify you as an individual, and you'll enter it to access this system."
+    )
+    username = inputs.username("Create a username: ")
+
+    # TODO: Check for dupe usernames
+
+    print(
+        "Your password is a secret phrase that you'll use to prove who you are when you log in"
+    )
+    print(
+        "Note that you won't be able to see your password while you're entering it"
+    )
+    password_hash = inputs.password("Set your password: ")
+
+    accounts.append({"username": username, "password_hash": password_hash})
+    accounts_database.save()
+    print()
+    print(
+        f"Created a new account called {Style.BRIGHT}{username}{Style.RESET_ALL}"
+    )
+
+
+init_colorama()
 accounts_database = JSONDatabase("accounts.json", [])
+accounts = accounts_database.data
+
+current_account = None
 
 main_menu = Menu(
     title="Mr Leeman's system",
     options=[
-        Option("Log in", log_in),
+        Option("Log in", log_in, lambda: not current_account),
+        Option("Create account", create_account),
         Submenu("Go to the sub menu", "Sub-menu",
                 [Option("Log in 1", log_in),
                  Option("Log in 2", log_in)])
